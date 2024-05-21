81217b87f75e6be189046aacf4a5e2466fa0559e
JSLEE <matt6009@naver.com>
2024-05-21 20:34:51 +0900

    Book-shop Project - app.js / 05.21

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv")
+dotenv.config()
+
+
+const bookRouter = require("./routes/books")
+const cartRouter = require("./routes/carts")
+const likeRouter = require("./routes/likes")
+const orderRouter = require("./routes/orders")
+const userRouter = require("./routes/users")
+
+
+app.use("/books",bookRouter)
+app.use("/carts",cartRouter)
+app.use("/likes",likeRouter)
+app.use("/orders",orderRouter)
+app.use("/users",userRouter)
+
+
+app.listen(process.env.PORT);
